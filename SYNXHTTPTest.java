789b419113811cd51ff747600dc0f6a6035e041d
stigottarjensen <stig@nornir.io>
2023-08-18 14:59:47 +0200

    uuyvyv

--- SYNXHTTPTest.java
+++ SYNXHTTPTest.java
@@ -39,45 +39,33 @@ import org.openxmlformats.schemas.drawingml.x2006.main.*;
 
 public class SYNXHTTPTest {
     public static Properties props = new Properties();
-    private static final String[] propFiles = { "StigZorro.xml", "SYNXParams2.xml" };
-    private static StringBuilder payload = new StringBuilder();
-    private static final ExecutorService executor = Executors.newSingleThreadExecutor();
+    // private static final String[] propFiles = { "StigZorro.xml",
+    // "SYNXParams2.xml" };
+    // private static final ExecutorService executor =
+    // Executors.newSingleThreadExecutor();
     AtomicBoolean notFinished = new AtomicBoolean(true);
-
-    public SYNXHTTPTest(String propFile) {
-        init(propFile);
-    }
-
-    private static void init(String propFile) {
-        try {
-            System.out.println("Propertis file = " + propFile);
-            props.loadFromXML(new FileInputStream(propFile));
-            payload.append("token=" + URLEncoder.encode(props.getProperty("token"), "UTF-8"));
-            // .append("txt=" + props.getProperty("txt")).append("&")
-            // .append("hei=" + props.getProperty("hei"));
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
 
     private static SSLSocketFactory sslsf;
 
-    private static void PostUrl(String SynxCat, String uri, String body, String melding) {
+    private static void PostUrl(String SynxCat, String uri, String body, String test) {
         try {
-
-            System.out.println("POST1");
-            String u = props.getProperty("httpUrl");
+            // URI gUri = new URI(uri);
+            // URL gurl = gUri.toURL();
+            // BufferedReader gbr = new BufferedReader(new
+            // InputStreamReader(gurl.openStream()));
+            // String g;
+            // while ((g = gbr.readLine()) != null) {
+            // System.out.println(g);
+            // Thread.sleep(100);
+            // }
+            // gbr.close();
+            System.out.println("*********************");
             URI Uri = new URI(uri);
             URL url = Uri.toURL();
-            StringBuilder payLoad = new StringBuilder();
             HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
             conn.setSSLSocketFactory(sslsf);
 
-            payLoad.append(SYNXHTTPTest.payload).append(body);
-            if (melding != null)
-                payLoad.append("&txt=" + melding + "&hei=" + melding);
-            String urlEncoded = URLEncoder.encode(payload.toString(), "UTF-8");
+            String urlEncoded = URLEncoder.encode(body, "UTF-8");
             conn.setRequestProperty("Synx-Cat", SynxCat);
             conn.setDoOutput(true);
             conn.setDoInput(true);
@@ -86,17 +74,22 @@ public class SYNXHTTPTest {
             osw.write(urlEncoded);
             osw.flush();
             osw.close();
-            System.out.println(SynxCat+" HTTP respons kode " + conn.getResponseCode());
-            conn.setReadTimeout(0);
+            System.out.println("SynxCat: " + SynxCat);
+            System.out.println("HTTP respons kode " + conn.getResponseCode() + " Test = " + test);
 
+            System.out.println("Start respons-------------------------");
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String line;
             while ((line = br.readLine()) != null) {
-                System.out.print("> " + line);
+                if (line.trim().isEmpty())
+                    System.out.println("Empty line with " + line.length() + " spaces");
+                else
+                    System.out.println(line);
             }
-
+            System.out.println("Slutt respons-------------------------");
             conn.disconnect();
             System.out.println("Ferdig SynxCat " + SynxCat);
+            System.out.println();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -104,8 +97,9 @@ public class SYNXHTTPTest {
     }
 
     public static void main(String[] args) throws Exception {
-
+        System.out.println("Før ssl");
         sslsf = SSLContext.getDefault().getSocketFactory();
+        System.out.println("Etter ssl");
 
         OPCPackage pkg = OPCPackage.open(new File("SynxCat_test_sheet.xlsx"));
         XSSFWorkbook wb = new XSSFWorkbook(pkg);
@@ -115,35 +109,44 @@ public class SYNXHTTPTest {
             int rowIndex = row.getRowNum();
             if (rowIndex < 4)
                 continue;
-            String SynxCat = "", Url = "", Token = "", RequestBody = "";
+            String SynxCat = "", Url = "", Token = "", RequestBody = "", Test = "";
             for (Cell cell : row) {
                 int colIndex = cell.getColumnIndex();
-                if (colIndex > 3)
+                if (colIndex > 5)
                     continue;
 
                 CellReference cellRef = new CellReference(rowIndex, colIndex);
 
                 String cellContent = formatter.formatCellValue(cell);
-                if (colIndex > 0 && colIndex < 4 && (cellContent == null || cellContent.length() < 1))
+                if (colIndex == 1 && !cellContent.equals("1"))
+                    break;
+                if (colIndex > 0 && colIndex < 5 && (cellContent == null || cellContent.length() < 1))
                     continue;
+
                 switch (colIndex) {
                     case 0:
                         SynxCat = cellContent;
                         break;
-                    case 1:
+                    case 2:
                         Url = cellContent;
                         break;
-                    case 2:
+                    case 3:
                         Token = cellContent;
                         break;
-                    case 3:
+                    case 4:
                         RequestBody = cellContent;
+                        break;
+                    case 5:
+                        Test = cellContent;
                         break;
                 }
             }
-            RequestBody = "token=" + Token + "&" + RequestBody;
-            if (Url.length()>0) {
-                SYNXHTTPTest.PostUrl(SynxCat, "https://" + Url, RequestBody, null);
+            RequestBody = (Token.length() > 0 ? "token=" + Token : "")
+                    + (RequestBody.length() > 0 ? "&" + RequestBody : "");
+            if (RequestBody.length() > 0 && RequestBody.charAt(0) == '&')
+                RequestBody = RequestBody.substring(1);
+            if (Url.length() > 0) {
+                SYNXHTTPTest.PostUrl(SynxCat, "https://" + Url, RequestBody, Test);
             }
         }
 
@@ -198,140 +201,146 @@ public class SYNXHTTPTest {
 // }
 // });
 
-    // private static void NioHTTPSocket(String SynxCat, String objectID, String melding) {
-    //     try {
-    //         System.out.println("NioSocket1");
-    //         StringBuilder payLoad = new StringBuilder();
-    //         payLoad.append(payload).append("&objectid=" + objectID);
-    //         if (melding != null)
-    //             payLoad.append("&txt=" + melding + "&hei=" + melding);
-    //         StringBuilder sb = new StringBuilder();
-    //         sb.append("POST /zorro HTTP/1.1\r\n");
-    //         sb.append("Host: stig.cioty.com\r\n");
-    //         sb.append("Synx-Cat: 4\r\n");
-    //         sb.append("Content-Length: " + payLoad.length() + "\r\n");
-    //         sb.append("Content-Type: application/x-www-form-urlencoded\r\n\r\n");
-    //         sb.append(payLoad.toString());
+// private static void NioHTTPSocket(String SynxCat, String objectID, String
+// melding) {
+// try {
+// System.out.println("NioSocket1");
+// StringBuilder payLoad = new StringBuilder();
+// payLoad.append(payload).append("&objectid=" + objectID);
+// if (melding != null)
+// payLoad.append("&txt=" + melding + "&hei=" + melding);
+// StringBuilder sb = new StringBuilder();
+// sb.append("POST /zorro HTTP/1.1\r\n");
+// sb.append("Host: stig.cioty.com\r\n");
+// sb.append("Synx-Cat: 4\r\n");
+// sb.append("Content-Length: " + payLoad.length() + "\r\n");
+// sb.append("Content-Type: application/x-www-form-urlencoded\r\n\r\n");
+// sb.append(payLoad.toString());
 
-    //         System.out.println(SynxCat + " " + payLoad);
-    //         // SocketFactory sf = SSLSocketFactory.getDefault();
-    //         // Socket so = sf.createSocket(" https://stig.cioty.com", 443);
-    //         InetSocketAddress address = new InetSocketAddress("stig.cioty.com", 443);
-    //         ByteBuffer buffer = ByteBuffer.allocate(4096);
-    //         SocketChannel sChannel = SocketChannel.open();
-    //         sChannel.connect(address);
-    //         sChannel.configureBlocking(false);
-    //         Selector selector = Selector.open();
-    //         sChannel.register(selector, SelectionKey.OP_WRITE);
-    //         sChannel.open();
-    //         final SSLEngine engine = SSLContext.getDefault().createSSLEngine();
-    //         engine.setUseClientMode(true);
-    //         engine.beginHandshake();
-    //         Charset charset = Charset.forName("UTF-8");
-    //         CharsetDecoder decoder = charset.newDecoder();
-    //         CharBuffer charBuffer;
-    //         while (true) {
-    //             selector.select();
-    //             Set<SelectionKey> keys = selector.selectedKeys();
-    //             Iterator<SelectionKey> it = keys.iterator();
-    //             while (it.hasNext()) {
-    //                 SelectionKey sk = it.next();
-    //                 if (sk.isWritable()) {
-    //                     SocketChannel sc = (SocketChannel) sk.channel();
-    //                     buffer.clear();
-    //                     buffer.put(sb.toString().getBytes(charset));
-    //                     buffer.flip();
-    //                     while (buffer.hasRemaining())
-    //                         sc.write(buffer);
-    //                     sc.register(selector, SelectionKey.OP_READ);
-    //                 }
-    //                 if (sk.isReadable()) {
-    //                     SocketChannel sc = (SocketChannel) sk.channel();
-    //                     buffer.clear();
-    //                     sc.read(buffer);
-    //                     String res = new String(buffer.array(), charset);
-    //                     System.out.println(res);
-    //                 }
-    //             }
-    //         }
-    //     } catch (Exception e) {
-    //         e.printStackTrace();
-    //     }
-    // }
+// System.out.println(SynxCat + " " + payLoad);
+// // SocketFactory sf = SSLSocketFactory.getDefault();
+// // Socket so = sf.createSocket(" https://stig.cioty.com", 443);
+// InetSocketAddress address = new InetSocketAddress("stig.cioty.com", 443);
+// ByteBuffer buffer = ByteBuffer.allocate(4096);
+// SocketChannel sChannel = SocketChannel.open();
+// sChannel.connect(address);
+// sChannel.configureBlocking(false);
+// Selector selector = Selector.open();
+// sChannel.register(selector, SelectionKey.OP_WRITE);
+// sChannel.open();
+// final SSLEngine engine = SSLContext.getDefault().createSSLEngine();
+// engine.setUseClientMode(true);
+// engine.beginHandshake();
+// Charset charset = Charset.forName("UTF-8");
+// CharsetDecoder decoder = charset.newDecoder();
+// CharBuffer charBuffer;
+// while (true) {
+// selector.select();
+// Set<SelectionKey> keys = selector.selectedKeys();
+// Iterator<SelectionKey> it = keys.iterator();
+// while (it.hasNext()) {
+// SelectionKey sk = it.next();
+// if (sk.isWritable()) {
+// SocketChannel sc = (SocketChannel) sk.channel();
+// buffer.clear();
+// buffer.put(sb.toString().getBytes(charset));
+// buffer.flip();
+// while (buffer.hasRemaining())
+// sc.write(buffer);
+// sc.register(selector, SelectionKey.OP_READ);
+// }
+// if (sk.isReadable()) {
+// SocketChannel sc = (SocketChannel) sk.channel();
+// buffer.clear();
+// sc.read(buffer);
+// String res = new String(buffer.array(), charset);
+// System.out.println(res);
+// }
+// }
+// }
+// } catch (Exception e) {
+// e.printStackTrace();
+// }
+// }
 
-    // private void HTTPPostSocket(String SynxCat, String objectID, String melding) {
-    //     Socket so;
-    //     try {
-    //         System.out.println("Socket1");
-    //         // if (melding==null) melding="EEE";
-    //         String u = props.getProperty("httpUrl");
+// private void HTTPPostSocket(String SynxCat, String objectID, String melding)
+// {
+// Socket so;
+// try {
+// System.out.println("Socket1");
+// // if (melding==null) melding="EEE";
+// String u = props.getProperty("httpUrl");
 
-    //         StringBuilder payLoad = new StringBuilder();
+// StringBuilder payLoad = new StringBuilder();
 
-    //         payLoad.append(this.payload).append("&objectid=" + objectID);
-    //         if (melding != null)
-    //             payLoad.append("&txt=" + melding + "&hei=" + melding);
-    //         System.out.println(SynxCat + " " + payLoad);
-    //         // String urlEncoded = URLEncoder.encode(payload.toString(), "UTF-8");
-    //         SocketFactory sf = SSLSocketFactory.getDefault();
-    //         so = sf.createSocket(" https://stig.cioty.com", 443);
-    //         so.setKeepAlive(true);
+// payLoad.append(this.payload).append("&objectid=" + objectID);
+// if (melding != null)
+// payLoad.append("&txt=" + melding + "&hei=" + melding);
+// System.out.println(SynxCat + " " + payLoad);
+// // String urlEncoded = URLEncoder.encode(payload.toString(), "UTF-8");
+// SocketFactory sf = SSLSocketFactory.getDefault();
+// so = sf.createSocket(" https://stig.cioty.com", 443);
+// so.setKeepAlive(true);
 
-    //         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(so.getOutputStream()));
-    //         StringBuilder sb = new StringBuilder();
-    //         sb.append("POST /zorro HTTP/1.1\r\n");
-    //         sb.append("Host: stig.cioty.com\r\n");
-    //         sb.append("Synx-Cat: 4\r\n");
-    //         sb.append("Content-Length: " + payLoad.length() + "\r\n");
-    //         sb.append("Content-Type: application/x-www-form-urlencoded\r\n\r\n");
-    //         sb.append(payLoad.toString());
-    //         bw.write(sb.toString());
-    //         System.out.println(sb);
-    //         bw.flush();
-    //         // token=aToken_124b34e931dd12fa57b28be8d56e6dff371cafe3570ab847e49f87012ff2eca0&objectid=2&txt=jadamasa&hei=jadamasa
+// BufferedWriter bw = new BufferedWriter(new
+// OutputStreamWriter(so.getOutputStream()));
+// StringBuilder sb = new StringBuilder();
+// sb.append("POST /zorro HTTP/1.1\r\n");
+// sb.append("Host: stig.cioty.com\r\n");
+// sb.append("Synx-Cat: 4\r\n");
+// sb.append("Content-Length: " + payLoad.length() + "\r\n");
+// sb.append("Content-Type: application/x-www-form-urlencoded\r\n\r\n");
+// sb.append(payLoad.toString());
+// bw.write(sb.toString());
+// System.out.println(sb);
+// bw.flush();
+// //
+// token=aToken_124b34e931dd12fa57b28be8d56e6dff371cafe3570ab847e49f87012ff2eca0&objectid=2&txt=jadamasa&hei=jadamasa
 
-    //         InputStream is = so.getInputStream();
-    //         BufferedReader br = new BufferedReader(new InputStreamReader(is));
+// InputStream is = so.getInputStream();
+// BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
-    //         String input = null;
-    //         for (int i = 0; i < 10; i++) {
-    //             while ((input = br.readLine()) != null) {
-    //                 System.out.println(input);
-    //             }
-    //             Thread.sleep(2000);
-    //         }
-    //         so.close();
-    //         System.out.println("Ferdig socket SynxCat " + SynxCat);
-    //     } catch (Exception e) {
-    //         e.printStackTrace();
-    //         System.out.println("Exception Ferdig socket SynxCat " + SynxCat);
-    //     }
-    // }
+// String input = null;
+// for (int i = 0; i < 10; i++) {
+// while ((input = br.readLine()) != null) {
+// System.out.println(input);
+// }
+// Thread.sleep(2000);
+// }
+// so.close();
+// System.out.println("Ferdig socket SynxCat " + SynxCat);
+// } catch (Exception e) {
+// e.printStackTrace();
+// System.out.println("Exception Ferdig socket SynxCat " + SynxCat);
+// }
+// }
 
-    // private static final List<CompletableFuture<Void>> asyncPushRequests = new CopyOnWriteArrayList<>();
+// private static final List<CompletableFuture<Void>> asyncPushRequests = new
+// CopyOnWriteArrayList<>();
 
-    // private static HttpResponse.PushPromiseHandler<String> pushPromiseHandler() {
+// private static HttpResponse.PushPromiseHandler<String> pushPromiseHandler() {
 
-    //     return (HttpRequest initiatingRequest,
-    //             HttpRequest pushPromiseRequest,
-    //             Function<HttpResponse.BodyHandler<String>, CompletableFuture<HttpResponse<String>>> acceptor) -> {
-    //         CompletableFuture<Void> pushcf = acceptor.apply(HttpResponse.BodyHandlers.ofString())
-    //                 .thenApply(HttpResponse::body)
-    //                 .thenAccept((b) -> System.out.println(
-    //                         "\nPushed resource body:\n " + b));
+// return (HttpRequest initiatingRequest,
+// HttpRequest pushPromiseRequest,
+// Function<HttpResponse.BodyHandler<String>,
+// CompletableFuture<HttpResponse<String>>> acceptor) -> {
+// CompletableFuture<Void> pushcf =
+// acceptor.apply(HttpResponse.BodyHandlers.ofString())
+// .thenApply(HttpResponse::body)
+// .thenAccept((b) -> System.out.println(
+// "\nPushed resource body:\n " + b));
 
-    //         asyncPushRequests.add(pushcf);
+// asyncPushRequests.add(pushcf);
 
-    //         System.out.println("\nJust got promise push number: " +
-    //                 asyncPushRequests.size());
-    //         System.out.println("\nInitial push request: " +
-    //                 initiatingRequest.uri());
-    //         System.out.println("Initial push headers: " +
-    //                 initiatingRequest.headers());
-    //         System.out.println("Promise push request: " +
-    //                 pushPromiseRequest.uri());
-    //         System.out.println("Promise push headers: " +
-    //                 pushPromiseRequest.headers());
-    //     };
-    // }
-
+// System.out.println("\nJust got promise push number: " +
+// asyncPushRequests.size());
+// System.out.println("\nInitial push request: " +
+// initiatingRequest.uri());
+// System.out.println("Initial push headers: " +
+// initiatingRequest.headers());
+// System.out.println("Promise push request: " +
+// pushPromiseRequest.uri());
+// System.out.println("Promise push headers: " +
+// pushPromiseRequest.headers());
+// };
+// }
